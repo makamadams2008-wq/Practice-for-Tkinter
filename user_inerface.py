1e9cb9fd71df321901260ce6ec3a17b9caffd8fb
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-04-01 08:39:27 +1300

    Apiary_Window added

--- user_inerface.py
+++ user_inerface.py
@@ -1,10 +1,9 @@
 import tkinter as tk
-from logic import test_func
-test_func()
+
 class Stats_Window:
     """Create the main window for tracking honey supplies"""
     def __init__(self, root):
-        """Initilise all of the varables for my stats window."""
+        """Initilise all of the varables for stats window."""
         self.current_honey_supply_label = tk.Label(text="Honey: 10/100000")
         self.current_honey_supply_label.pack()
 
@@ -21,18 +20,58 @@ class Stats_Window:
 class Config_Window:
     """Update & create values for the bees, hives, and apiary"""
     def __init__(self, root):
-        """Initilise all of the varables for my connfig window."""
-        self.current_honey_supply_label = tk.Label(text="Honey: 10/100000")
-        self.current_honey_supply_label.pack()
+        """Initilise all of the varables for connfig window."""
+        self.nav_bar_frame = tk.Frame()
+        self.nav_bar_frame.pack()
 
-        self.current_week_label = tk.Label(text="Week 0")
-        self.current_week_label.pack()
+        self.apairy_frame = tk.Frame()
+        self.apairy_frame.pack()
+        
+        self.worker_bee_frame = tk.Frame()
+        self.worker_bee_frame.pack()
+
+        self.queen_bee_frame = tk.Frame()
+        self.queen_bee_frame.pack()
+
+        self.footer_frame = tk.Frame()
+        self.footer_frame.pack()
+
+class Apiary_Window:
+    """Create a window for controlling the different hives"""
+    def __init__(self, root):
+        """Initilise all of the varables for apiary window."""
+        self.apairy_setup_frame = tk.Frame()
+        self.apairy_setup_frame.pack()
+
+        self.apairy_config_frame = tk.Frame()
+        self.apairy_config_frame.pack()
+
+        self.enviroment_config_frame = tk.Frame()
+        self.enviroment_config_frame.pack()
+
+        self.active_hives_frame = tk.Frame()
+        self.active_hives_frame.pack()
+
+        self.add_hive_frame = tk.Frame()
+        self.add_hive_frame.pack()
 
         self.new_week_button = tk.Button(text= "New Week -->", command = self.new_week)
         self.new_week_button.pack()
 
-    def new_week(self):
-        """Update all the values for the next week"""
+    def apiary_config(self):
+        """Configure the apairy"""
+        pass
+
+    def initial_setup(self):
+        """Initilise the apairy"""
+        pass
+    
+    def config_enviroment(self):
+        """Configure the enviroment"""
+        pass
+
+    def add_hive(self):
+        """Add a new hive to the apiary"""
         pass
 
 
