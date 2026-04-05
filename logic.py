44c56acd7f0bdba9d526586e092258313e43b847
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-04-05 18:49:51 +1200

    added hive class

--- logic.py
+++ logic.py
@@ -24,5 +24,32 @@ class Apiary:
     def create_hive(self):
         pass
 
+    def kill_hive(self):
+        pass
 
 
+class Hive:
+
+    def __init__(self, name, population):
+
+        self.name = name
+        self. alive = True
+        self.population = population 
+        self.hive_energy = 1
+        self.honey_stocks = 0
+        self. honey_capasity = 100
+
+    def forage_for_honey(self):
+        pass
+
+    def hibernate(self):
+        pass
+
+    def incress_population(self):
+        pass
+
+    def harvest_honey(self):
+        pass
+    
+    def create_queen(self):
+        pass
